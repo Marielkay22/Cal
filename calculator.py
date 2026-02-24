57b7e5a29a105796b2316ffc84f0a48d2356d06d
Marielkay22 <banaag.marielkay@gmail.com>
2026-02-24 18:27:53 +0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,36 @@
+def marielkaye():
+    print("SELECT OPERATION:")
+    print("(1)Add")
+    print("(2)Subtract")
+    print("(3)Multiply")
+    print("(4)Divide\n")
+
+    user = int(input("Enter Choices: ")) 
+    if user == 1:
+        a = int(input("enter 1st number: "))
+        b = int(input("enter 2nd number: "))
+        result = a+b
+        print("Result is: ",result)
+
+    elif user == 2:
+        a = int(input("enter 1st number: "))
+        b = int(input("enter 2nd number; "))
+        result = a-b
+        print("Result is: ",result)
+
+    elif user == 3:
+        a = int(input("enter 1st number: "))
+        b = int(input("enter 2nd number: "))
+        result = a*b
+        print("Result is: ",result)
+
+    elif user == 4:
+        a = int(input("enter 1st number: "))
+        b = int(input("enter 2nd number: "))
+        result = a/b
+        print("Result is: ",result)
+    else:
+        print("wrong password")
+    
+    
+marielkaye()
